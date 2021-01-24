bde250c20c159b19c8889a2ccb9536540ee84200
Flavio Rocha <flavio_271295@hotmail.com>
2021-01-24 12:02:07 -0300

    add to filter a client

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -12,11 +12,25 @@ function List() {
   const { clientsData, leadsData, screen } = useClienteData();
   let search = screen.searchBar.list.value;
   const clientRender = () => {
-    const render =
-      clientsData.length > 0 &&
-      clientsData.map((client, index) => (
-        <ClientInfo key={client.id} client={client} />
-      ));
+    const render = !search
+? clientsData.map((client, index) => (
+    <ClientInfo key={client.id} client={client} />
+  ))
+: clientsData
+    .filter((client) => {
+      if (search === "") {
+        return client;
+      } else if (
+        client.commercial_name
+          .toLowerCase()
+          .includes(search.toLowerCase())
+      ) {
+        return client;
+      }
+    })
+    .map((client, key) => {
+      return <ClientInfo key={client.id} client={client} />;
+    });
     return render;
   };
   const leadRender = () => {
